6c1cad6ede2f2a61b8461d79f54d1f5633a71797
Brijesh Prajapati <bprajapati@deloitte.com>
2021-03-19 16:42:13 -0400

    Initial Load

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,14 +1,14 @@
-//resource "aws_security_group" "es" {
-//  name = "${local.common_prefix}-commonuse-sg"
-//  description = "Security group for Common Use"
-//  vpc_id = aws_vpc.demo.id
-//
-//  ingress {
-//      from_port = 0
-//      to_port = 0
-//      protocol = "-1"
-//      cidr_blocks = [
-//          aws_vpc.demo.cidr_block
-//      ]
-//  }
-//}
+resource "aws_security_group" "es" {
+  name = "${local.common_prefix}-commonuse-sg"
+  description = "Security group for Common Use"
+  vpc_id = aws_vpc.demo.id
+
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [
+          aws_vpc.demo.cidr_block
+      ]
+  }
+}
