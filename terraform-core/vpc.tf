0fcf10931b52f84789086dcbd46521177b180ff2
Brijesh Prajapati <bprajapati@deloitte.com>
2021-03-19 17:09:03 -0400

    Initial Load

--- terraform-core/vpc.tf
+++ terraform-core/vpc.tf
@@ -8,11 +8,11 @@ resource "aws_vpc" "demo" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.id
+  value = aws_vpc.demo.id
 }
 
 output "vpc_cidr" {
-  value = aws_vpc.cidr_block
+  value = aws_vpc.demo.cidr_block
 }
 
 resource "aws_subnet" "public_1" {
