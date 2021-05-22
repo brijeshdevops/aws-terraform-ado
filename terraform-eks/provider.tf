36011f4251a9a46e4d3a4f6550024ad7af95cc1d
Brijesh Prajapati <bprajapati@deloitte.com>
2021-05-21 23:29:45 -0400

    Initial Load

--- terraform-eks/provider.tf
+++ terraform-eks/provider.tf
@@ -51,7 +51,13 @@ locals {
   cluster_name = "${var.project_id}-${var.env_type}-EKS-Cluster"
 }
 
-
+provider "kubernetes" {
+  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, [""]), 0)
+  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, [""]), 0))
+  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, [""]), 0)
+  load_config_file       = false
+  version                = "1.10"
+}
 
 //provider "kubernetes" {
 //  host                   = data.aws_eks_cluster.cluster.endpoint
