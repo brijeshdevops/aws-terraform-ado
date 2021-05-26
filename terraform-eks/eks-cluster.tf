6b25449318a159ff252ea140c37196da549f369d
Brijesh Prajapati <bprajapati@deloitte.com>
2021-05-26 15:40:22 -0400

    Initial Load

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -13,26 +13,10 @@ data "aws_eks_cluster_auth" "cluster" {
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
 
-//provider "kubernetes" {
-//  host                   = data.aws_eks_cluster.cluster.endpoint
-//  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-//  token                  = data.aws_eks_cluster_auth.cluster.token
-//  load_config_file       = false
-//}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      data.aws_eks_cluster.cluster.name
-    ]
-  }
 }
 
 module "eks" {
@@ -68,8 +52,7 @@ module "eks" {
       instance_type    = var.eks_intance_type_main
       key_name         = var.node_ssh_key
       source_security_group_ids = [
-        aws_security_group.main_security_group.id,
-        aws_security_group.worker_group_mgmt_one.id
+        aws_security_group.main_security_group.id
       ]
 
       subnets = data.aws_subnet_ids.eks_subnets_private.ids
--- terraform-eks/security-groups.tf
+++ terraform-eks/security-groups.tf
@@ -6,6 +6,7 @@ resource "aws_security_group" "main_security_group" {
     from_port = 0
     to_port   = 65535
     protocol  = "tcp"
+    self      = true
 
     cidr_blocks = [
       #"${var.vpc_cidr}",
@@ -14,7 +15,8 @@ resource "aws_security_group" "main_security_group" {
       "172.16.0.0/12",
       "192.168.0.0/16",
       "167.219.0.0/16",
-      "104.190.188.27/32" # My Home IP
+      "104.190.188.27/32", # My Home IP
+      "10.0.2.65/32"
     ]
   }
 
@@ -37,7 +39,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/21", "104.190.188.27/32", "192.168.0.0/16", "167.219.0.0/16"
+      "10.0.0.0/21", "104.190.188.27/32", "192.168.0.0/16", "167.219.0.0/16", "10.0.2.65/32"
     ]
   }
 
@@ -59,7 +61,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "192.168.0.0/16", "104.190.188.27/32", "167.219.0.0/16"
+      "192.168.0.0/16", "104.190.188.27/32", "167.219.0.0/16", "10.0.2.65/32"
     ]
   }
 
@@ -85,7 +87,8 @@ resource "aws_security_group" "all_worker_mgmt" {
       "172.16.0.0/12",
       "192.168.0.0/16",
       "104.190.188.27/32",
-      "167.219.0.0/16"
+      "167.219.0.0/16",
+      "10.0.2.65/32"
     ]
   }
 
