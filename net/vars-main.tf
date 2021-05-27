89f6a187e18292b56c6557cf246ce441d21c111c
Brijesh Prajapati <bprajapati@deloitte.com>
2021-05-27 18:02:00 -0400

    Initial Load

---
+++ net/vars-main.tf
@@ -0,0 +1,13 @@
+# TF_VAR_region
+variable "region" {
+  description = "The name of the AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster-name" {
+  description = "The name of the EKS Cluster"
+  type        = string
+  default     = "mycluster1"
+}
+ 
